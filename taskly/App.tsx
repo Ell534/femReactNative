1daed5a85d6d1f77b77c3018dc33f543122ece03
Ell534 <elliot94b@gmail.com>
2024-10-05 20:44:10 +0100

    begin styling first component

--- taskly/App.tsx
+++ taskly/App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: "#1a759f",
+          paddingHorizontal: 8,
+          paddingVertical: 16,
+        }}
+      >
+        <Text style={{ fontSize: 18, fontWeight: "200", }}>Coffee</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });
