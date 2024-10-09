502d9bc3141a4067034064b846b905b1b28fd8df
Ell534 <elliot94b@gmail.com>
2024-10-09 17:46:25 +0100

    complete UI for shopping list component

---
+++ taskly/components/ShoppingListItem.tsx
@@ -0,0 +1,81 @@
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native"
+import { theme } from "../theme";
+
+type Props = {
+  name: string;
+  isCompleted?: boolean;
+}
+
+export function ShoppingListItem({ name, isCompleted }: Props) {
+
+  const handleDelete = () => {
+    Alert.alert(
+      `Are you sure you want to delete ${name}?`,
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    )
+  }
+
+  return (
+    <View style={[styles.itemContainer, isCompleted ? styles.completedContainer : undefined]}>
+      <Text style={[styles.itemText, isCompleted ? styles.completedText : undefined]}>{name}</Text>
+      <TouchableOpacity style={[styles.button, isCompleted ? styles.completedButton : undefined]} onPress={handleDelete} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorGrey,
+    color: theme.colorGrey,
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  completedButton: {
+    backgroundColor: theme.colorGrey,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
+});
